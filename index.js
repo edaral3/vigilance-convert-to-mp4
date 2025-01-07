963e575c5c6c604a36ae0044b8791c97eecedb1c
edaral3 <36478825+edaral3@users.noreply.github.com>
2025-01-07 04:39:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-const path = require('path');
-const fs = require('fs');
+import fs from 'fs';
+import AWS from 'aws-sdk';
 
-const AWS = require('aws-sdk');
+const INPUT = '/mnt/efs/hls'
+const BUCKET_NAME = 'vigilance-records';
+
 const s3 = new AWS.S3();
 
-const BUCKET_NAME = 'vigilance-records';
-const REGION = 'us-east-1';
-
-const INPUT = '/efs/hls'
-
 const concatTSFiles = async (dir, name) => {
-  try {
-    const tsFiles = fs.readdirSync(`${INPUT}/${dir}`).filter(file => file.endsWith('.ts')).slice(20);
-    console.log(tsFiles)
-    if(tsFiles.length < 10) return
-    
-    let bufferVideo = [];
-    for (const tsFile of tsFiles) {
-      const readStream = fs.readFileSync(`${INPUT}/${dir}/${tsFile}`);
-      bufferVideo.push(readStream)
+    try {
+        const tsFiles = fs.readdirSync(`${INPUT}/${dir}`).filter(file => file.endsWith('.ts')).slice(20, 608);
+        if (tsFiles.length < 10) return
+
+        let bufferVideo = [];
+        tsFiles.slice(0, 588)
+        for (const tsFile of tsFiles) {
+            const readStream = fs.readFileSync(`${INPUT}/${dir}/${tsFile}`);
+
+            bufferVideo.push(readStream)
+        }
+
+        const data = {
+            Bucket: BUCKET_NAME,
+            Key: `${dir}/${name}.mp4`,
+            Body: Buffer.concat(bufferVideo),
+            ContentType: 'video/mp4',
+        };
+        await s3.putObject(data).promise();
+    } catch (error) {
+        console.log('error', error)
     }
+}
 
-    const data = {
-      Bucket: BUCKET_NAME,
-      Key: `${dir}/${name}.mp4`,
-      Body: Buffer.concat(bufferVideo),
-      ContentType: 'video/mp4',
+export const handler = async (event) => {
+    const directories = fs.readdirSync(INPUT);
+
+    const now = new Date();
+    now.setHours(now.getHours() - 6);
+    const dateString = now.toISOString().split('T')[0];
+
+    for (const dir of directories) await concatTSFiles(dir, dateString);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify('File operation successful!'),
     };
-    
-    const asd = await s3.putObject(data).promise();
-  } catch (error) {
-    console.log('error', error)
-  }
-}
-
-const main = async () => {
-  const directories = fs.readdirSync(INPUT);
-
-  const now = new Date();
-  now.setHours(now.getHours() - 6);
-  const dateString = now.toISOString().split('T')[0];
-
-  for (const dir of directories) {
-    concatTSFiles(dir, dateString);
-  }
-}
-
-main();
+};
